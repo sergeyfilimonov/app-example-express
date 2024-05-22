370884cac1044f1b96d49aa12877915587085ee3
Sergey Filimonov <43918077+sergeyfilimonov@users.noreply.github.com>
2024-05-22 09:30:03 +0300

    1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Timeweb Cloud + Express = ️ ❤️')
-})
+})//
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
